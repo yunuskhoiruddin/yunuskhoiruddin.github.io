6de3784678ac1ffbe1adb987623d81e90817e6dd
M Yunus Khoiruddin <askumuntukanda@gmail.com>
2024-07-20 11:06:05 +0800

    Update Database.

--- batch_resize.py
+++ batch_resize.py
@@ -1,7 +1,7 @@
 import os
 from PIL import Image
 
-def resize_images_in_folder(source_folder, target_folder, target_width=500):
+def resize_images_in_folder(source_folder, target_folder, target_width=400):
     os.makedirs(target_folder, exist_ok=True)
     for filename in os.listdir(source_folder):
         if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
@@ -10,8 +10,17 @@ def resize_images_in_folder(source_folder, target_folder, target_width=500):
                 width_percent = (target_width / float(img.size[0]))
                 target_height = int((float(img.size[1]) * float(width_percent)))
                 img = img.resize((target_width, target_height), Image.ANTIALIAS)
-                target_path = os.path.join(target_folder, filename)
-                img.save(target_path)
+                
+                # Convert image to RGB if not already in that mode (to ensure compatibility with JPG format)
+                if img.mode != 'RGB':
+                    img = img.convert('RGB')
+                
+                # Change extension to .jpg for output files
+                base_filename = os.path.splitext(filename)[0]
+                target_filename = f"{base_filename}.jpg"
+                target_path = os.path.join(target_folder, target_filename)
+                
+                img.save(target_path, format='JPEG')
                 print(f'{filename} resized and saved to {target_path}')
 
 source_folder = 'assets/projects'
